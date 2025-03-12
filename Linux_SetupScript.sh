590b46f27376d7252f5d0e125aaaf44c993074c5
Mohammed Moosa <157305945+cliffsandroses@users.noreply.github.com>
2025-03-12 14:43:27 +0530

    Update Linux_SetupScript.sh
    Some additional lines added

--- Linux_SetupScript.sh
+++ Linux_SetupScript.sh
@@ -25,23 +25,41 @@ choice $flavour in
 	if [ "$systype" == 1]
 		then
   		echo "Syncing Pacman in"
-		for i in 5 4 3 2 1
-		do echo "$i . . "
-		sleep 1
+		for i in 3 2 1
+   			do echo "$i . . "
+			sleep 1
 		done
-  		sudo pacman -Syyu -y
-		sudo pacman -S --needed base-devel vi vim nano git flatpak ttf-liberation-mono-nerd wezterm obsidian docker flameshot libreoffice-fresh docker-buildx python python-pynput python-pip python-virtualenv python-setuptools -y
+  		echo "Now"
+  		sudo pacman -Syy -y
+    		echo
+    		echo "Installing applications."
+		echo
+  		sudo pacman -S --needed base-devel vim nano git flatpak ttf-liberation-mono-nerd wezterm obsidian docker flameshot libreoffice-fresh docker-buildx python python-pynput python-pip python-virtualenv python-setuptools -y
+    		echo
+      		echo "Package Installation from pacman is done!"
+		echo
+  		echo "Installing packages from Flatpak in"
+		for i in 3 2 1
+   			do echo "$i . . "
+			sleep 1
+		done
+  		echo "Now"
 		sudo flatpak install brave zed nomacs syncthingy vscodium heroicgameslauncher -y # nomacs - image viewer
-		git clone https://aur.archlinux.org/yay.git
+		git clone https://aur.archlinux.org/yay.git # installing yay
 		cd yay
 		makepkg -si -y
 		yay -Sua
-		yay -S docker-desktop
+		yay -S docker-desktopx
+  		sudo groupadd docker # adding docker to group
+    		sudo usermod -aG docker $USER
+      		newgrp docker
+		sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+		sudo chmod g+rwx "$HOME/.docker" -R
 	
 	elif [ "$systype" == 2]
 		then
 		sudo pacman -Syyu -y
-		sudo pacman -S --needed base-devel vi vim nano git flatpak ttf-liberation-mono-nerd wezterm obsidian docker flameshot libreoffice-fresh docker-buildx python python-pynput python-pip python-virtualenv python-setuptools -y
+		sudo pacman -S --needed base-devel vim git flatpak ttf-liberation-mono-nerd wezterm obsidian docker flameshot libreoffice-fresh docker-buildx python python-pynput python-pip python-virtualenv python-setuptools -y
 		sudo flatpak install brave zed nomacs syncthingy vscodium heroicgameslauncher -y
 
 	elif [ "$systype" == 3 ]
@@ -49,16 +67,18 @@ choice $flavour in
 		sudo pacman -S open-vm-tools ttf-liberation-mono-nerd wezterm -y
 		sudo systemctl status vmtoolsd.service
 		for i in 5 4 3 2 1
-		do echo "$i . . "
-		sleep 1
+			do echo "$i . . "
+			sleep 1
 		done
+  		echo "Done!"
 		sudo systemctl enable vmtoolsd.service
 		sudo systemctl start vmtoolsd.service
 		sudo systemctl status vmtoolsd.service
 		for i in 5 4 3 2 1
-		do echo "$i . . "
-		sleep 1
+			do echo "$i . . "
+			sleep 1
 		done
+  		echo "Done!"
   	fi
    ;;
 
